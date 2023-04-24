f60cbaeae4006deab9f953ea0994f4b9ab6f7f7c
Cranky Bawler <6666lenia6666@mail.ru>
2023-04-24 19:38:20 +0300

    Шаг 4

---
+++ src/main/java/pro/sky/Collections/sheets/and/sets/EmployeeService.java
@@ -0,0 +1,20 @@
+package pro.sky.Collections.sheets.and.sets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class EmployeeService {
+    List<Employee> employees = List.of(
+            new Employee("Иван", "Иванов"),
+            new Employee("Тихон", "Гуляев"),
+            new Employee("Савелий", "Шашков"),
+            new Employee("Остап", "Николаев"),
+            new Employee("Алексей", "Шаров"),
+            new Employee("Дарина", "Вишнякова"),
+            new Employee("Астра", "Калашникова"),
+            new Employee("Роксана", "Беспалова"),
+            new Employee("Хильда", "Лазарева"),
+            new Employee("Дана", "Уварова")
+    );
+}
