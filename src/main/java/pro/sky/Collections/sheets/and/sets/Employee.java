3ca2db7b40c50e6c8aae5c5c9194b4c51d2bffce
Cranky Bawler <6666lenia6666@mail.ru>
2023-04-27 17:53:18 +0300

    1

--- src/main/java/pro/sky/Collections/sheets/and/sets/Employee.java
+++ src/main/java/pro/sky/Collections/sheets/and/sets/Employee.java
@@ -1,5 +1,7 @@
 package pro.sky.Collections.sheets.and.sets;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Employee {
---
+++ src/main/java/pro/sky/Collections/sheets/and/sets/exception/EmployeeAlredyAddedException.java
@@ -0,0 +1,26 @@
+package pro.sky.Collections.sheets.and.sets.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class EmployeeAlredyAddedException extends RuntimeException{
+    public EmployeeAlredyAddedException() {
+    }
+
+    public EmployeeAlredyAddedException(String message) {
+        super(message);
+    }
+
+    public EmployeeAlredyAddedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EmployeeAlredyAddedException(Throwable cause) {
+        super(cause);
+    }
+
+    public EmployeeAlredyAddedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
